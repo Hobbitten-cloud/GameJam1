d7ad951019b10e1e7af9521fce82ba045cdafc3f
Nicklas Hansen <nicklashansen2121@gmail.com>
2024-10-21 14:45:44 +0200

    Add new enemy skeleton

--- Program.cs
+++ Program.cs
@@ -6,12 +6,14 @@ namespace gamejam1
     {
         static void Main(string[] args)
         {
+            // Instanser 
             Player bob = new Player("bob", 5, "sword", "", true, 10, 10, 5, 2, 1, 2);
+            Quiz question1 = new Quiz("How many hands does a human have?", "1", "2", "4", 1);
+            Enemy skeleton = new Enemy("Skeleton", 10, "1", "", true);
 
             List<Room> rooms = new List<Room>();
 
-            rooms.Add(new Room(new Trial("Abe?","Leg",new Quiz(),new Enemy(),new Loot()),0,"Room 1:", "Hej?"));
-
+            rooms.Add(new Room(new Trial("Abe?", "Leg", question1, skeleton, new Loot()), 0, "Room 1:", "Hej?"));
 
 
             Controller controller = new Controller(new Menu(), new Floor(0, rooms), bob);

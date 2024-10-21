7d4d1be41a2f2c2bae0b1a75b24f97fed8a5d637
Nicklas Hansen <nicklashansen2121@gmail.com>
2024-10-21 08:05:26 +0200

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace gamejam1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Gamejam test");
+        }
+    }
+}

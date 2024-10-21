4bf0b0e97179a50640420acc415d5cbcecb188e7
Dennis <Dennisjensen9452@gmail.com>
2024-10-21 13:32:32 +0200

    add Quiz

--- Quiz.cs
+++ Quiz.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace gamejam1
 {
-    internal class Quiz
+    public class Quiz
     {
+       public string Spørgmål { get; }
+
+       public string Svar_1 {  get; }
+       public string Svar_2 {  get; }
+       public string Svar_3 { get; }
+       public  int Rigtigsvar {  get; }
+
+        public Quiz(string spørgmål, string svar_1, string svar_2, string svar_3,int rigtigsvar) 
+        {
+            Spørgmål = spørgmål;
+            Svar_1 = svar_1;
+            Svar_2 = svar_2;
+            Svar_3 = svar_3;
+            Rigtigsvar = rigtigsvar;
+
+        }
+
+
+
     }
 }
